8a95a0d5892f80f15aeda47bcb913d834a835afc
Ronald M. Clifford <roncli@roncli.com>
2019-03-17 19:29:37 -0700

    Fix bug when missing server information.
    Fixes #8.

--- web/views/home.js
+++ web/views/home.js
@@ -35,7 +35,7 @@ class Home {
      * @returns {Promise} A promise that resolves when the request is complete.
      */
     static async get(req, res) {
-        const servers = await Servers.servers,
+        const servers = (await Servers.servers).filter((s) => servers[s].gameStarted && servers[s].map && servers[s].name && servers[s].maxNumPlayers),
             now = new Date();
 
         const html = Common.page(/* html */`
@@ -59,7 +59,7 @@ class Home {
                 ${Object.keys(servers).filter((s) => now.getTime() - servers[s].lastSeen.getTime() <= 60 * 60 * 1000).sort((a, b) => servers[a].name.localeCompare(servers[b].name)).map((s) => /* html */`
                     <div>${Common.htmlEncode(servers[s].name)}</div>
                     <div>${s}</div>
-                    <div>${Common.htmlEncode(servers[s].map)}</div>
+                    <div><a target="_blank" href="https://overloadmaps.com/${encodeURI(servers[s].map.toLowerCase().replace(/[ _]/g, "-"))}">${Common.htmlEncode(servers[s].map)}</a></div>
                     <div>${Common.htmlEncode(servers[s].mode)}</div>
                     <div>${servers[s].numPlayers}/${servers[s].maxNumPlayers}</div>
                     <div><time class="timeago" datetime="${new Date(servers[s].lastSeen).toISOString()}">${new Date(servers[s].lastSeen)}</time></div>
