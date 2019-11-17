24af8644de1a52e1eff5da3c9fefaa08fa30933c
Ronald M. Clifford <roncli@roncli.com>
2019-11-17 10:45:13 -0800

    Case fix.

--- public/views/gamelist/game.js
+++ public/views/gamelist/game.js
@@ -24,14 +24,14 @@ class GameView {
     static get(game) {
         return /* html */`
             <div><a href="/archive/${game.id}">${GameView.Common.formatDate(new Date(game.date))}</a></div>
-            <div>${GameView.Common.HtmlEncode(game.server && game.server.name || game.server && game.server.ip || game.ip || "Unknown")}</div>
-            <div>${game.data && game.data.settings && GameView.Common.HtmlEncode(game.data.settings.matchMode) || ""}</div>
-            <div>${game.data && game.data.settings && GameView.Common.HtmlEncode(game.data.settings.level) || ""}</div>
+            <div>${GameView.Common.htmlEncode(game.server && game.server.name || game.server && game.server.ip || game.ip || "Unknown")}</div>
+            <div>${game.data && game.data.settings && GameView.Common.htmlEncode(game.data.settings.matchMode) || ""}</div>
+            <div>${game.data && game.data.settings && GameView.Common.htmlEncode(game.data.settings.level) || ""}</div>
             <div>
                 ${game.data && game.data.teamScore && Object.keys(game.data.teamScore).length > 0 && Object.keys(game.data.teamScore).sort((a, b) => game.data.teamScore[b] - game.data.teamScore[a]).slice(0, 4).map((team) => /* html */`
-                    ${GameView.Common.HtmlEncode(team)} ${game.data.teamScore[team]}
+                    ${GameView.Common.htmlEncode(team)} ${game.data.teamScore[team]}
                 `.trim()).join(", ") || game.data && game.data.players && game.data.players.length > 0 && game.data.players.sort((a, b) => b.kills * (game.data.players.length > 2 ? 3 : 1) + b.assists - (a.kills * (game.data.players.length > 2 ? 3 : 1) + a.assists)).slice(0, 4).map((player) => /* html */`
-                    ${GameView.Common.HtmlEncode(player.name)} ${player.kills * (game.data.players.length > 2 ? 3 : 1) + player.assists}
+                    ${GameView.Common.htmlEncode(player.name)} ${player.kills * (game.data.players.length > 2 ? 3 : 1) + player.assists}
                 `.trim()).join(", ") || ""}${game.data && game.data.teamScore && game.data.teamScore.length > 4 || game.data && (!game.data.teamScore || game.data.teamScore.length === 0) && game.data.players && game.data.players.length > 4 ? ", ..." : ""}
             </div>
         `;
