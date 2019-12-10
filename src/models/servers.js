8649b94b8c73c33228700c10f547e5ca0cf0e53d
Ronald M. Clifford <roncli@roncli.com>
2019-12-09 20:01:19 -0800

    Use local cache for servers.
    Fixes #29.

--- src/models/servers.js
+++ src/models/servers.js
@@ -2,6 +2,8 @@ const Db = require("../database/servers"),
     Game = require("./game"),
     Websocket = require("../websocket");
 
+let servers = [];
+
 //   ###
 //  #   #
 //  #       ###   # ##   #   #   ###   # ##    ###
@@ -25,10 +27,15 @@ class Servers {
      * @returns {Promise<object[]>} A promise that resolves with the servers.
      */
     static async getVisible() {
-        const servers = await Db.getVisible(),
-            now = new Date();
+        const now = new Date();
 
-        servers.forEach((server) => {
+        if (servers.length === 0) {
+            servers = await Db.getVisible();
+        }
+
+        const serverInfo = JSON.parse(JSON.stringify(servers));
+
+        serverInfo.forEach((server) => {
             const game = Game.getByIp(server.ip);
 
             if (game && game.inLobby && game.settings) {
@@ -56,7 +63,7 @@ class Servers {
             server.old = now.getTime() - new Date(server.lastSeen).getTime() > 60 * 60 * 1000;
         });
 
-        return servers.filter((s) => s.keepListed && new Date().getTime() - new Date(s.lastSeen).getTime() < 30 * 24 * 60 * 60 * 1000 || new Date().getTime() - new Date(s.lastSeen).getTime() < 60 * 60 * 1000);
+        return serverInfo.filter((s) => s.keepListed && new Date().getTime() - new Date(s.lastSeen).getTime() < 30 * 24 * 60 * 60 * 1000 || new Date().getTime() - new Date(s.lastSeen).getTime() < 60 * 60 * 1000);
     }
 
     //                #         #
@@ -74,10 +81,25 @@ class Servers {
      * @returns {Promise} A promise that resolves when the server has been updated.
      */
     static async update(ip, data, visible) {
-        let server = await Db.getByIp(ip);
+        let server = servers.find((s) => s.ip === ip);
 
-        if (!server.ip) {
-            server = {ip};
+        if (!server) {
+            server = await Db.getByIp(ip);
+
+            if (!server || !server.ip) {
+                server = {ip};
+            }
+
+            if (visible) {
+                servers.push(server);
+            }
+        }
+
+        if (!visible) {
+            const index = servers.indexOf(server);
+            if (index !== -1) {
+                servers.splice(index, 1);
+            }
         }
 
         Object.keys(data).forEach((key) => {
