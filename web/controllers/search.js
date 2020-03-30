10e90c65f0a9342145e7fbbebdb4de2615842ea8
Ronald M. Clifford <roncli@roncli.com>
2020-03-30 12:37:34 -0700

    Fix search controller as well as API.

--- web/controllers/search.js
+++ web/controllers/search.js
@@ -32,8 +32,14 @@ class Search {
      * @returns {Promise} A promise that resolves when the request is complete.
      */
     static async get(req, res) {
-        const q = (req.query.q || "").substr(0, 100),
-            {games, count} = await Completed.search(q, 1);
+        const q = (req.query.q || "").substr(0, 100);
+
+        let games, count;
+        if (!q || q.trim().length === 0) {
+            ({games, count} = await Completed.getList(1));
+        } else {
+            ({games, count} = await Completed.search(q, 1));
+        }
 
         res.status(200).send(Common.page(
             "",
