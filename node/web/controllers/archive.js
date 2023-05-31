9b0e419e9206fb10f405b1803c3db63a37a6ea9f
Ronald M. Clifford <roncli@roncli.com>
2023-05-30 17:06:23 -0700

    Fixes from main branch.

--- node/web/controllers/archive.js
+++ node/web/controllers/archive.js
@@ -82,6 +82,9 @@ class Archive extends RouterBase {
         let weapons = [];
         if (game.damage) {
             weapons = game.damage.map((d) => d.weapon).filter((w, index, arr) => arr.indexOf(w) === index).sort((a, b) => Weapon.orderedWeapons.indexOf(a) - Weapon.orderedWeapons.indexOf(b));
+
+            // Only include damage where the defender is known.
+            game.damage = game.damage.filter((d) => game.players.find((p) => p.name === d.defender));
         }
 
         let gameLength = game.settings && game.settings.timeLimit && game.settings.timeLimit || 0;
